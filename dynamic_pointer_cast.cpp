e030d539ca378e4cf0278cc9f0dd0aae19ddcde4
swordfatih <35528737+swordfatih@users.noreply.github.com>
2018-12-08 19:41:20 +0100

    Add files via upload

---
+++ dynamic_pointer_cast.cpp
@@ -0,0 +1,35 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <memory>
+
+int main()
+{
+    sf::RenderWindow window({940, 480}, "Window");
+
+    std::vector<std::shared_ptr<sf::Drawable>> objects;
+
+    objects.push_back(std::make_shared<sf::CircleShape>(30));
+
+    std::dynamic_pointer_cast<sf::Transformable>(objects[0])->setPosition(50, 50);
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        for(auto& it: objects)
+        {
+            window.draw(*it);
+        }
+        window.display();
+    }
+
+    return 0;
+}
