c8e48f90a82a3267b2b306dfef145a1806172d8e
swordfatih <35528737+swordfatih@users.noreply.github.com>
2018-12-08 19:12:21 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+using drawableTransformable = std::pair<sf::Drawable*, sf::Transformable*>;
+
+int main()
+{
+    sf::RenderWindow window({940, 480}, "Window");
+
+    std::vector<drawableTransformable> objects;
+
+    sf::CircleShape shape(40);
+    objects.push_back(drawableTransformable(&shape, &shape));
+
+    std::get<sf::Transformable*>(objects[0])->setPosition(50, 50);
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        for(auto& it: objects)
+        {
+            window.draw(*std::get<sf::Drawable*>(it));
+        }
+        window.display();
+    }
+
+    return 0;
+}
